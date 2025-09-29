a342beb54ff3277ea5747645fdc2dcebe8f08994
22205858 <kevin.kolosiana@gmail.com>
2025-09-29 15:15:42 +0200

    commentaire sur les semaphores

--- src/Affichage.java
+++ src/Affichage.java
@@ -10,11 +10,13 @@ import java.lang.String;
 
 public class Affichage extends Thread{
 	String texte;
-    static semaphoreBinaire semaphoreBinaire = new semaphoreBinaire(1);
+    //on utilise static pour que tous les threads est le même semaphore
+    static semaphore semaphoreBinaire = new semaphoreBinaire(1);
 
 	public Affichage (String txt){texte=txt;}
 	public void run(){
         semaphoreBinaire.syncWait();
+        //la boucle for est la section critique
         for (int i=0; i<texte.length(); i++){
             //sout est la ressource critique
             System.out.print(texte.charAt(i));
