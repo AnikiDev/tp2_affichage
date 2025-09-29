f20fe53a6a7964f83b1584db8da80fd348c598c1
22205858 <kevin.kolosiana@gmail.com>
2025-09-29 14:53:10 +0200

    utilisation des semaphoreBinaire

--- src/Affichage.java
+++ src/Affichage.java
@@ -7,22 +7,19 @@ import java.io.Reader;
 import java.io.BufferedReader;
 import java.lang.String;
 
-class Exclusion{};
 
 public class Affichage extends Thread{
-	String texte; 
+	String texte;
+    static semaphoreBinaire semaphoreBinaire = new semaphoreBinaire(1);
 
 	public Affichage (String txt){texte=txt;}
 	public void run(){
-        synchronized (System.out) {
-            //toute la boucle for est la section critique
-            for (int i=0; i<texte.length(); i++){
-                //sout est la ressource critique
-                System.out.print(texte.charAt(i));
-                try {sleep(100);} catch(InterruptedException e){};
-            }
+        semaphoreBinaire.syncWait();
+        for (int i=0; i<texte.length(); i++){
+            //sout est la ressource critique
+            System.out.print(texte.charAt(i));
+            try {sleep(100);} catch(InterruptedException e){};
         }
-
-
-	}
+        semaphoreBinaire.syncSignal();
+    }
 }
